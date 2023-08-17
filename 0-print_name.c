300fad2f3edb64d607ceb1d2f29b79cb8d104df9
Ttobss <tansontobs21@gmail.com>
2023-08-17 14:23:49 -0700

    last

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,17 +1,28 @@
 #include "function_pointers.h"
 
+#include <stdio.h>
+
 /**
- * print_name - prints a name
- * @name: name's main
- * @f: void function
- */
+
+ * print_name - print name using pointer to function
+
+ * @name: string to add
+
+ * @f: pointer to function
+
+ * Return: nothing
+
+ **/
 
 void print_name(char *name, void (*f)(char *))
+
 {
-	if (name && f)
-	{
-		f(name);
-	}
+
+        if (name == NULL || f == NULL)
+
+                return;
+
+
+        f(name);
+
 }
-
-
